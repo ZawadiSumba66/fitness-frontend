80981c5daa5da12575604424413e95710500d9b1
ZawadiSumba66 <rosesumba98@gmail.com>
2021-07-05 22:35:35 +0300

    Basic action setup

--- src/actions/action.js
+++ src/actions/action.js
@@ -1,1 +1,20 @@
+import { CREATE_USER, CREATE_USER_ERROR } from "./type";
 import axios from "axios";
+
+const createUser = newUser => async(dispatch)=>{
+    try {
+    dispatch({ type: CREATE_USER, ...newUser });
+    const response = await axios({
+        method: 'POST',
+        url: 'http://localhost:3000/api/v1/users',
+        data: { user: newUser },
+        crossdomain: true,
+    });
+    const { token } = response.data;
+		localStorage.setItem('jwt', token);	
+	} catch {
+		dispatch({ type: CREATE_USER_ERROR });
+	}
+}
+
+export default createUser
