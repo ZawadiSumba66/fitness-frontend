be68cc7268df58d548e5684d1659f9d6ee54320f
ZawadiSumba66 <rosesumba98@gmail.com>
2021-07-05 22:36:58 +0300

    connect the signup to redux

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,11 +1,39 @@
 import { connect } from 'react-redux';
+import authReducer from '../reducers/user';
 
-const SignUp =()=>{
+const SignUp =({ newUser, user, history })=>{
     const [userName, setUserName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [passwordConfirmation, setPasswordConfirmation] = useState('')
     const [avatar, setAvatar] = useState('')
+    const [message, setMessage] = useState('')
+
+    const handleSubmit = async(e) =>{
+      e.preventDefualt()
+		const imgPath = await uploadFile(avatar);
+		avatar = imgPath;
+		await newUser({username, email, password,passwordConfirmation, avatar});
+		if (user.isLogin === true) {
+			history.push('/');
+		} else {
+			setMessage('welcome');
+            return message
+		} 
+    }
+
+    const uploadFile = async(imageFile) => {
+       const formData = new FormData();
+		formData.append('file', imageFile);
+		formData.append('upload_preset', 'avatar');
+		const response = await axios({
+			url: 'http://localhost:3000/rails/active_storage/direct_uploads',
+			method: 'POST',
+			data: formData,
+		});
+		return response.data.secure_url;
+    }
+
  return(
      <form onSubmit={handleSubmit}>
     <input 
@@ -38,7 +66,7 @@ const SignUp =()=>{
     />
       <input 
       value={avatar}
-      onChange={(e)=> setAvatar(e.target.value)}
+      onChange={(e)=> setAvatar(e.target.files[0])}
       type="file"
       name="avatar"
     />
@@ -49,4 +77,11 @@ const SignUp =()=>{
     )  
 }
 
-export default SignUp
+const mapStateToProps = state => ({
+	user: state.authReducer.user,
+});
+const mapDispatchToProps = dispatch => ({
+	newUser: estate => dispatch(authReducer(estate)),
+});
+
+export default connect(mapStateToProps,mapDispatchToProps)(SignUp)
